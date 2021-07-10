5be635893673ada6756c59f564c918e2399bcc3f
akshatarun <akshatarun17@gmail.com>
2021-07-10 20:29:07 +0530

    Task 5.5
      - Add AngularJs with default view as home.html in the popup window
      - Use bodyparser instead of express.json for parsing request body

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
-$.ajax({
-    type: "POST",
-    url: "http://localhost:5000/products",
-    data: { testData: "Hi i am the test data from the ajax post request call" },
-    success: function(response){
-        console.log(response.data);
-    }
-});
+chrome.runtime.onInstalled.addListener(()=>{
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:5000/products",
+        data: { testData: "Hi i am the test data from the ajax post request call" },
+        success: function(response){
+            console.log(response.data);
+        }
+    });
+})
--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const config = require('./config.js')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const PORT = 5000;
 const app = express()
 
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
+// app.use(
+//     express.urlencoded({
+//         extended: true
+//     })
+// )
   
-app.use(express.json())
+// app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+// shorter way for the above code. It is a middleware which parses request's body into js objects.
+// middlewares are executed for all urls in the beginning
+
+mongoose.connect(config.url,{ useNewUrlParser: true ,useUnifiedTopology: true}).then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
 
 app.get('/', (req,res)=>{
     res.send("Hi")
