3bf8c2d2e0023c70c395226d04e888090941c7cd
akshatarun <akshatarun17@gmail.com>
2021-07-12 16:52:44 +0530

    Task-6
      - create basic login and signup forms
      - create the states and controllers for login and signup
      - transfer the forms data to the console of popup.js

--- popup.js
+++ popup.js
@@ -1,14 +1,39 @@
 let amazonextension = angular.module("amazonextension", ['ui.router']).config([
-    '$stateProvider',
-    '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-    
-      $stateProvider
-        .state('home', {
-          url: '/home',
-          templateUrl: '/home.html',
-          controller: 'MainCtrl'
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+  
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        //controller: 'MainCtrl'
+      }).state('login', {
+          url: '/login',
+          templateUrl: '/login.html',
+          controller: 'LoginController'
+        }).state('signup', {
+          url: '/signup',
+          templateUrl: '/signup.html',
+          controller: 'SignupController'
         });
-    
-      $urlRouterProvider.otherwise('home');
-    }])
+  
+    $urlRouterProvider.otherwise('login');
+  }
+]);
+
+amazonextension.controller('LoginController', function($scope){
+  $scope.username="";
+  $scope.password="";
+  $scope.login = function(){
+    console.log("login's console",$scope.username,$scope.password);
+  }
+});
+
+amazonextension.controller('SignupController', function($scope){
+  $scope.username="";
+  $scope.password="";
+  $scope.signup = function(){
+    console.log("signup's console",$scope.username,$scope.password);
+  }
+});
